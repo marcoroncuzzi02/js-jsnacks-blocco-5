079e0338d981db2659f0dfa57e6816545ba438b7
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-27 21:53:35 +0100

    ADD ex 3 e 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,8 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const autori = posts.map( (element) => {
+  return (element.author)
+})
+console.log(autori)
