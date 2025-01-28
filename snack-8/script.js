42fa47fbd9078b5b798c91f56496bf2d2175086f
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-01-28 18:37:15 +0100

    END

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student = students.find ( (element) => (element.name === `Marco Lanci`) )
+
+console.log(student.class)
